8316c56254c62be57b86895274a09c09ce06f303
miinjaekim <minjae.kim@uni.minerva.edu>
2025-05-02 12:05:39 -0700

    updated requirements to use minimum version specifications and added VapiReservationTool to the tools module

--- Voyagent/tools/__init__.py
+++ Voyagent/tools/__init__.py
@@ -2,11 +2,13 @@ from Voyagent.tools.perplexity import PerplexitySearchTool
 from Voyagent.tools.apify import ApifyFlightTool, ApifyPOITool
 from Voyagent.tools.deepl import DeepLTranslateTool
 from Voyagent.tools.rime import RimeReservationTool
+from Voyagent.tools.vapi import VapiReservationTool
 
 __all__ = [
     'PerplexitySearchTool',
     'ApifyFlightTool',
     'ApifyPOITool',
     'DeepLTranslateTool',
-    'RimeReservationTool'
+    'RimeReservationTool',
+    'VapiReservationTool'
 ]
